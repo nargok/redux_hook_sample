412c75444889f25ce771b6486d6b62ae4c51df89
nargok <nargok@gmail.com>
2020-01-18 20:12:36 +0900

    use redux hook

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import reducer from "./veicle";
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const vehicle = createStore(reducer);
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={vehicle}>
+    <App />
+  </Provider>,
+  rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
